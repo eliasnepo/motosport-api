4115e52cc5aa632d6cf626748e4ff746fa4012ff
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-07-09 17:29:34 -0300

    Changed generate id strategy to identity

--- app/src/main/java/com/eliasnepo/motosport/infraestructure/cars/jpa/CarEntity.java
+++ app/src/main/java/com/eliasnepo/motosport/infraestructure/cars/jpa/CarEntity.java
@@ -21,7 +21,7 @@ import java.time.LocalDate;
 public class CarEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- app/src/main/java/com/eliasnepo/motosport/infraestructure/category/jpa/CategoryEntity.java
+++ app/src/main/java/com/eliasnepo/motosport/infraestructure/category/jpa/CategoryEntity.java
@@ -19,7 +19,7 @@ import javax.persistence.*;
 public class CategoryEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
