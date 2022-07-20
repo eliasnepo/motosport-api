421c488bea14120e3bf61a3df3d2d2a7f113f25a
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-07-20 00:50:46 -0300

    Added AuthComponent to load the user based on his token

---
+++ app/src/main/java/com/eliasnepo/motosport/infraestructure/config/security/utils/AuthComponent.java
@@ -0,0 +1,27 @@
+package com.eliasnepo.motosport.infraestructure.config.security.utils;
+
+import com.eliasnepo.motosport.infraestructure.user.jpa.UserEntity;
+import com.eliasnepo.motosport.infraestructure.user.jpa.UserRepositoryJpa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class AuthComponent {
+
+    @Autowired
+    private UserRepositoryJpa userRepository;
+
+    @Transactional(readOnly = true)
+    public UserEntity authenticated() {
+        try {
+            String email = SecurityContextHolder.getContext().getAuthentication().getName();
+            return userRepository.findByEmail(email).get();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Invalid user");
+        }
+    }
+}
