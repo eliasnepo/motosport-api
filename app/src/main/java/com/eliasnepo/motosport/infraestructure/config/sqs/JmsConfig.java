afcad2ac093313c3617cf79274f07150d580486d
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-07-21 00:43:12 -0300

    Added SQS Consumer configuration

---
+++ app/src/main/java/com/eliasnepo/motosport/infraestructure/config/sqs/JmsConfig.java
@@ -0,0 +1,39 @@
+package com.eliasnepo.motosport.infraestructure.config.sqs;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+
+import javax.jms.Session;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    private SQSConnectionFactory sqsConnectionFactory;
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        sqsConnectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard()
+                        .withRegion("sa-east-1")
+                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+                        .build());
+
+        DefaultJmsListenerContainerFactory factory =
+                new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("2");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+
+        return factory;
+    }
+}
